55a5637a19f4b191fe3352f4686a57403322e3b5
Smoothingo <127873986+Smoothingo@users.noreply.github.com>
2023-04-12 22:59:19 +0200

    ver-1.11

--- Zelda_Botw_Kattner/modules/func.py
+++ Zelda_Botw_Kattner/modules/func.py
@@ -21,7 +21,7 @@ def ssep_line():
 def bsep_line():
     print("═"*100)
 
-inventory = {
+personal_inventory = {
     "weapons": {
         "sword": {"qty": 2, "atk": 10, "price": 15},
         "axe": {"qty": 1, "atk": 12, "price": 15},
@@ -69,13 +69,13 @@ purse = {"💰 Rupees": 30}
 def show_balance(purse):
     print(f"You're total Rupees are {purse['💰 Rupees']}")
 
-def buy_item(item, shop_inventory, purse):
+def buy_item(item, shop_personal_inventory, purse):
     excluded_items = ["map"]
     if item in excluded_items:
         print("You can't buy this item! 🚫")
         return False
     item_data = None
-    for category, items in shop_inventory.items():
+    for category, items in shop_personal_inventory.items():
         if item in items:
             item_data = items[item]
             break
@@ -96,19 +96,19 @@ def buy_item(item, shop_inventory, purse):
     purse['💰 Rupees'] -= total_price
     item_data['qty'] -= qty_to_buy
     if item_data['qty'] == 0:
-        del shop_inventory[category][item]
-        if not shop_inventory[category]:
-            del shop_inventory[category]
+        del shop_personal_inventory[category][item]
+        if not shop_personal_inventory[category]:
+            del shop_personal_inventory[category]
     print(f"You bought {qty_to_buy} {item}(s)! 🛍️")
     return True
 
-def sell_item(item, shop_inventory, purse):
+def sell_item(item, shop_personal_inventory, purse):
     excluded_items = ["map"]
     if item in excluded_items:
         print("You can't sell this item! 🚫")
         return False
     item_data = None
-    for category, items in shop_inventory.items():
+    for category, items in shop_personal_inventory.items():
         if item in items:
             item_data = items[item]
             break
@@ -130,22 +130,22 @@ def sell_item(item, shop_inventory, purse):
 
 
 
-def dis_inventory(shop_inventory, category):
+def dis_personal_inventory(shop_personal_inventory, category):
     print(f"{category.capitalize()}:")
-    for item in shop_inventory[category]:
-        qty = shop_inventory[category][item]['qty']
-        price = shop_inventory[category][item]['price']
+    for item in shop_personal_inventory[category]:
+        qty = shop_personal_inventory[category][item]['qty']
+        price = shop_personal_inventory[category][item]['price']
         stats = ""
-        if 'atk' in shop_inventory[category][item]:
-            stats += f" ATK: {shop_inventory[category][item]['atk']}"
-        if 'def' in shop_inventory[category][item]:
-            stats += f" DEF: {shop_inventory[category][item]['def']}"
-        if 'health_bonus' in shop_inventory[category][item]:
-            stats += f" HP: {shop_inventory[category][item]['health_bonus']}"
+        if 'atk' in shop_personal_inventory[category][item]:
+            stats += f" ATK: {shop_personal_inventory[category][item]['atk']}"
+        if 'def' in shop_personal_inventory[category][item]:
+            stats += f" DEF: {shop_personal_inventory[category][item]['def']}"
+        if 'health_bonus' in shop_personal_inventory[category][item]:
+            stats += f" HP: {shop_personal_inventory[category][item]['health_bonus']}"
         print(f"{item.capitalize()}: {qty} available for {price} 💰 per unit.{stats}")
         
 
-def shop_interact(shop_inventory, purse):
+def shop_interact(shop_personal_inventory, purse):
     while True:
         print("Welcome to the Shop! What do you want to do?")
         print("[B] - Buy something")
@@ -156,9 +156,9 @@ def shop_interact(shop_inventory, purse):
 
         if action == "b":
             show_balance(purse)
-            print_shop_inventory(shop_inventory)
+            print_shop_personal_inventory(shop_personal_inventory)
             item = input("What do you want to buy: ").lower()
-            if buy_item(item, shop_inventory, purse):
+            if buy_item(item, shop_personal_inventory, purse):
                 print(f"You have {purse['💰 Rupees']} Rupees.")
                 a = input("Would you like to continue shopping or safe Hyrule before its to late? (shop/safe) ").lower()
                 if a == "shop":
@@ -171,9 +171,9 @@ def shop_interact(shop_inventory, purse):
                 print(f"Im sorry, {item} could not be bought.")
         elif action == "s":
             show_balance(purse)
-            print_shop_inventory(shop_inventory)
+            print_shop_personal_inventory(shop_personal_inventory)
             item = input("What do you want to sell: ").lower()
-            if sell_item(item, shop_inventory, purse):
+            if sell_item(item, shop_personal_inventory, purse):
                 print(f"You have {purse['💰 Rupees']} Rupees.")
                 a = input("Would you like to continue shopping or safe Hyrule before its to late? (shop/safe) ").lower()
                 if a == "shop":
@@ -188,41 +188,41 @@ def shop_interact(shop_inventory, purse):
             print("Good bye! Enjoy your life young Adventurer!")
 
 
-   # show_inventory(shop_inventory)
-def print_shop_inventory(shop_inventory):
-    # print the header for the inventory
+   # show_personal_inventory(shop_personal_inventory)
+def print_shop_personal_inventory(shop_personal_inventory):
+    # print the header for the personal_inventory
     print("          Shop Items:")
     print("================================")
-    # print the weapons section of the inventory
+    # print the weapons section of the personal_inventory
     print("🗡 Weapons:")
     # iterate through each weapon and its attributes (quantity and attack power)
-    for k, v in shop_inventory["weapons"].items():
+    for k, v in shop_personal_inventory["weapons"].items():
         # print the name of the weapon, its quantity, and its attack power
-        print(f"\t• {k}: {v['qty']} ({v['atk']} attack)")
-    # print the food section of the inventory
+        print(f"\t• {k}: {v['qty']} ({v['atk']} attack) ({v['price']} 💰 Rupees each)")
+    # print the food section of the personal_inventory
     print("\n🍔 Food:")
     # iterate through each food item and its attributes (quantity and health bonus)
-    for k, v in shop_inventory["food"].items():
+    for k, v in shop_personal_inventory["food"].items():
         # print the name of the food item, its quantity, and its health bonus
-        print(f"\t• {k}: {v['qty']} ({v['health_bonus']} health bonus)")
-    # print the defense section of the inventory
+        print(f"\t• {k}: {v['qty']} ({v['health_bonus']} health bonus) ({v['price']} 💰 Rupees each)")
+    # print the defense section of the personal_inventory
     print("\n🛡 Defense:")
     # iterate through each defense item and its attributes (quantity and defense power)
-    for k, v in shop_inventory["defense"].items():
+    for k, v in shop_personal_inventory["defense"].items():
         # print the name of the defense item, its quantity, and its defense power
-        print(f"\t• {k}: {v['qty']} ({v['def']} defense)")
-    # print the items section of the inventory
+        print(f"\t• {k}: {v['qty']} ({v['def']} defense) ({v['price']} 💰 Rupees each)")
+    # print the items section of the personal_inventory
     print("\n🧰 Items:")
     # iterate through each item and its attributes (quantity, health bonus, or defense power)
-    for k, v in shop_inventory["items"].items():
+    for k, v in shop_personal_inventory["items"].items():
         # check if the item has a health bonus attribute
         if 'health_bonus' in v:
             # print the name of the item, its quantity, and its health bonus
-            print(f"\t• {k}: {v['qty']} ({v['health_bonus']} health bonus)")
+            print(f"\t• {k}: {v['qty']} ({v['health_bonus']} health bonus) ({v['price']} 💰 Rupees each)")
         # check if the item has a defense power attribute
         elif 'def' in v:
             # print the name of the item, its quantity, and its defense power
-            print(f"\t• {k}: {v['qty']} ({v['def']} defense)")
+            print(f"\t• {k}: {v['qty']} ({v['def']} defense) ({v['price']} 💰 Rupees each)")
         # if the item has neither health bonus nor defense power, it is a regular item
         else:
             # print the name of the item and its quantity
@@ -230,48 +230,48 @@ def print_shop_inventory(shop_inventory):
 
 
 
-# This function takes an inventory dictionary as its input and prints the contents of the inventory in a formatted manner.
-def print_inventory(inventory):
-    # print the header for the inventory
-    print("          Personal Inventory:")
+# This function takes an personal_inventory dictionary as its input and prints the contents of the personal_inventory in a formatted manner.
+def print_personal_inventory(personal_inventory):
+    # print the header for the personal_inventory
+    print("          Personal personal_inventory:")
     print("================================")
-    # print the weapons section of the inventory
+    # print the weapons section of the personal_inventory
     print("🗡 Weapons:")
     # iterate through each weapon and its attributes (quantity and attack power)
-    for k, v in inventory["weapons"].items():
+    for k, v in personal_inventory["weapons"].items():
         # print the name of the weapon, its quantity, and its attack power
-        print(f"\t• {k}: {v['qty']} ({v['atk']} attack)")
-    # print the food section of the inventory
+        print(f"\t• {k}: {v['qty']} ({v['atk']} attack) ({v['price']} 💰 Rupees each)")
+    # print the food section of the personal_inventory
     print("\n🍔 Food:")
     # iterate through each food item and its attributes (quantity and health bonus)
-    for k, v in inventory["food"].items():
+    for k, v in personal_inventory["food"].items():
         # print the name of the food item, its quantity, and its health bonus
-        print(f"\t• {k}: {v['qty']} ({v['health_bonus']} health bonus)")
-    # print the defense section of the inventory
+        print(f"\t• {k}: {v['qty']} ({v['health_bonus']} health bonus) ({v['price']} 💰 Rupees each)")
+    # print the defense section of the personal_inventory
     print("\n🛡 Defense:")
     # iterate through each defense item and its attributes (quantity and defense power)
-    for k, v in inventory["defense"].items():
+    for k, v in personal_inventory["defense"].items():
         # print the name of the defense item, its quantity, and its defense power
-        print(f"\t• {k}: {v['qty']} ({v['def']} defense)")
-    # print the items section of the inventory
+        print(f"\t• {k}: {v['qty']} ({v['def']} defense) ({v['price']} 💰 Rupees each)")
+    # print the items section of the personal_inventory
     print("\n🧰 Items:")
     # iterate through each item and its attributes (quantity, health bonus, or defense power)
-    for k, v in inventory["items"].items():
+    for k, v in personal_inventory["items"].items():
         # check if the item has a health bonus attribute
         if 'health_bonus' in v:
             # print the name of the item, its quantity, and its health bonus
-            print(f"\t• {k}: {v['qty']} ({v['health_bonus']} health bonus)")
+            print(f"\t• {k}: {v['qty']} ({v['health_bonus']} health bonus) ({v['price']} 💰 Rupees each)")
         # check if the item has a defense power attribute
         elif 'def' in v:
             # print the name of the item, its quantity, and its defense power
-            print(f"\t• {k}: {v['qty']} ({v['def']} defense)")
+            print(f"\t• {k}: {v['qty']} ({v['def']} defense) ({v['price']} 💰 Rupees each)")
         # if the item has neither health bonus nor defense power, it is a regular item
         else:
             # print the name of the item and its quantity
             print(f"\t• {k}: {v['qty']}")
 
 if __name__ == "__main__":
-    print_inventory(inventory)
+    print_personal_inventory(personal_inventory)
 
 
 def zelda_themesound(start_time=0):
@@ -321,9 +321,9 @@ def print_health():
 def print_defense():
     print("Defense: " + str(defense), "🛡️ "*defense)
 
-# Define function to print the inventory, current health and shield status.
+# Define function to print the personal_inventory, current health and shield status.
 def print_status(): 
-    print_inventory(inventory)
+    print_personal_inventory(personal_inventory)
     print("================================")
     print_health()
     print_defense()
